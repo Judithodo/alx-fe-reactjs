068a17605ae66e1f37def9b657cc210a3ff210fa
Odo Judith <odojudith18@gmail.com>
2025-02-18 04:24:41 +0100

    add new updates

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
