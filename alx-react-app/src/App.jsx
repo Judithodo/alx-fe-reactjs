54210f328c5fd3e93c0cc5b35b7f31cf2b9f6316
Odo Judith <odojudith18@gmail.com>
2025-02-18 04:33:55 +0100

    add update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -23,6 +23,28 @@ function App() {
 export default App;
 
 
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      <UserProfile 
+        name="Bob" 
+        age="30" 
+        bio="Enjoys coding and playing video games" 
+      />
+    </div>
+  );
+}
+
+export default App;
+
 
 
 function App() {
