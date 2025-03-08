9be3c63a82d9b90a8c902f33984089684adb01f0
Odo Judith <odojudith18@gmail.com>
2025-03-08 18:56:15 +0100

    add update

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,4 +1,20 @@
 // src/components/DeleteRecipeButton.jsx
+// import { useRecipeStore } from '../recipeStore';
+
+// const DeleteRecipeButton = ({ recipeId }) => {
+//   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+
+//   const handleDelete = () => {
+//     deleteRecipe(recipeId);
+//   };
+
+//   return <button onClick={handleDelete}>Delete Recipe</button>;
+// };
+
+// export default DeleteRecipeButton;
+
+
+// src/components/DeleteRecipeButton.jsx
 import { useRecipeStore } from '../recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
